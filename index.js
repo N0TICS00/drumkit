8d9fa1f068dfd03a049b8c79ae9124102eb9c0dd
N0TICS00 <76558194+N0TICS00@users.noreply.github.com>
2021-03-12 09:05:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,38 +7,38 @@ function handleClick() {
   var buttonInnerHTML = this.innerHTML;
   switch (buttonInnerHTML) {
     case "w":
-      var tom1 = new Audio('sounds/tom-1.mp3');
+      var tom1 = new Audio('tom-1.mp3');
       tom1.play();
 
       break;
       case "a":
-      var tom2 = new Audio ('sounds/tom-2.mp3');
+      var tom2 = new Audio ('tom-2.mp3');
       tom2.play();
       break;
 
       case "s":
-      var tom3 = new Audio ('sounds/tom-3.mp3');
+      var tom3 = new Audio ('tom-3.mp3');
       tom3.play();
       break;
 
       case "d":
-      var tom4 = new Audio ('sounds/tom-4.mp3');
+      var tom4 = new Audio ('tom-4.mp3');
       tom4.play();
       break;
 
 
       case "j":
-      var snare = new Audio ('sounds/snare.mp3');
+      var snare = new Audio ('snare.mp3');
       snare.play();
       break ;
 
       case "k":
-      var crash = new Audio('sounds/crash.mp3');
+      var crash = new Audio('crash.mp3');
       crash.play();
       break;
 
       case "l":
-      var kickBass = new Audio ('sounds/kick-bass.mp3');
+      var kickBass = new Audio ('kick-bass.mp3');
       kickBass.play();
     default:
     console.log(buttonInnerHTML)
@@ -59,7 +59,7 @@ switch (event.key) {
 
 case "w":
 
-    var tom1 = new Audio('sounds/tom-1.mp3');
+    var tom1 = new Audio('tom-1.mp3');
 
     tom1.play();
 
@@ -69,7 +69,7 @@ case "w":
 
 case "a":
 
-    var tom2 = new Audio('sounds/tom-2.mp3');
+    var tom2 = new Audio('tom-2.mp3');
 
     tom2.play();
 
@@ -79,7 +79,7 @@ case "a":
 
   case "s":
 
-      var tom3 = new Audio('sounds/tom-3.mp3');
+      var tom3 = new Audio('tom-3.mp3');
 
       tom3.play();
 
@@ -89,7 +89,7 @@ case "a":
 
   case "d":
 
-      var tom4 = new Audio('sounds/tom-4.mp3');
+      var tom4 = new Audio('tom-4.mp3');
 
       tom4.play();
 
@@ -99,7 +99,7 @@ case "a":
 
   case "j":
 
-      var snare = new Audio('sounds/snare.mp3');
+      var snare = new Audio('snare.mp3');
 
       snare.play();
 
@@ -109,7 +109,7 @@ case "a":
 
   case "k":
 
-      var crash = new Audio('sounds/crash.mp3');
+      var crash = new Audio('crash.mp3');
 
       crash.play();
 
@@ -119,7 +119,7 @@ case "a":
 
   case "l":
 
-      var kickBass = new Audio('sounds/kick-bass.mp3');
+      var kickBass = new Audio('kick-bass.mp3');
 
       kickBass.play();
 
